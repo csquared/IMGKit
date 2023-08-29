782b206dceed41172881fe6ea0462fac059f18fa
Michael Treacher <treacher@github.com>
2023-08-29 02:48:39 +0000

    Use IO.popen instead of `` for checking if wkhtmltoimage exists

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -7,7 +7,7 @@ describe IMGKit::Configuration do
       let(:system_path_with_bundler_warning) { "`/` is not writable.\nBundler will use `/tmp/bundler/home/unknown' as your home directory temporarily.\n/path/to/wkhtmltoimage\n" }
 
       before(:each) do
-        allow(subject).to receive(:`).and_return(system_path)
+        allow(IO).to receive(:popen).and_return(double(read: system_path))
       end
 
       context "with Bundler" do
@@ -16,17 +16,17 @@ describe IMGKit::Configuration do
         end
 
         it "should return the result of `bundle exec which wkhtmltoimage` with whitespace stripped" do
-          expect(subject).to receive(:`).with("bundle exec which wkhtmltoimage")
+          expect(IO).to receive(:popen).with(%w(bundle exec which wkhtmltoimage))
           expect(subject.wkhtmltoimage).to eq system_path.chomp
         end
 
         context "with warning" do
           before(:each) do
-            allow(subject).to receive(:`).and_return(system_path_with_bundler_warning)
+            allow(IO).to receive(:popen).and_return(double(read: system_path_with_bundler_warning))
           end
 
           it "should return the result of `bundle exec which wkhtmltoimage` with warning stripped" do
-            expect(subject).to receive(:`).with("bundle exec which wkhtmltoimage")
+            expect(IO).to receive(:popen).with(%w(bundle exec which wkhtmltoimage))
             expect(subject.wkhtmltoimage).to eq system_path.chomp
           end
         end
@@ -38,7 +38,7 @@ describe IMGKit::Configuration do
         end
 
         it "should return the result of `which wkhtmltoimage` with whitespace stripped" do
-          expect(subject).to receive(:`).with("which wkhtmltoimage")
+          allow(IO).to receive(:popen).with(%w("which wkhtmltoimage))
           expect(subject.wkhtmltoimage).to eq system_path.chomp
         end
       end
@@ -46,7 +46,7 @@ describe IMGKit::Configuration do
 
     context "system version does not exist" do
       before(:each) do
-        allow(subject).to receive(:`).and_return("\n")
+        allow(IO).to receive(:popen).and_return(double(read: "\n"))
         allow(subject).to receive(:using_bundler?).and_return(false)
       end
 
@@ -63,7 +63,7 @@ describe IMGKit::Configuration do
       end
 
       it "should not check the system version and return the explicit path" do
-        expect(subject).to_not receive(:`)
+        expect(IO).to_not receive(:popen)
         expect(subject.wkhtmltoimage).to eq explicit_path
       end
     end
